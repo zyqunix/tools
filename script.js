ed0b2f5e66e5d7c32ca8d8d2edd093e4ab592fa3
zyqunix <fabiovanek2008@gmail.com>
2024-10-29 20:08:28 +0100

    commit

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const themeToggle = document.getElementById('themeToggle');
+const topbar = document.getElementById('topbar')
 
 let isLightMode = false;
 
 themeToggle.addEventListener('click', function() {
     if (!isLightMode) {
-
-        /*
-            LIGHT MODE
-            LIGHT MODE
-            LIGHT MODE
-        */
-
         document.body.style.backgroundColor = '#C8C8C8';
         container.style.backgroundColor = "#B0B0B0";
         themeToggle.innerHTML = `<svg viewBox="0 0 24 24" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -29,27 +23,13 @@ themeToggle.addEventListener('click', function() {
             </g></svg>`;
         container.style.color = "#1E1E1E"
         isLightMode = true;
-        document.getElementById('github').innerHTML = `<svg width="48px" height="48px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#141414" style="--darkreader-inline-fill: #141414;" data-darkreader-inline-fill=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>github [#142]</title><desc>Created with Sketch.</desc><defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="f4f4f4" fill-rule="evenodd" style="--darkreader-inline-stroke: none;" data-darkreader-inline-stroke=""> <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)" fill="#141414" style="--darkreader-inline-fill: #000000;" data-darkreader-inline-fill=""> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399" id="github-[#142]"> </path> </g> </g> </g> </g></svg>    `;
 
         let children = container.children;
         for (let i = 0; i < children.length; i++) {
             children[i].style.color = "#1E1E1E";
         }
 
-        rmbReset.style.backgroundColor = "#1E1E1E";
-        lmbReset.style.backgroundColor = "#1E1E1E";
-        rmbReset.style.color = "#b0b0b0";
-        lmbReset.style.color = "#b0b0b0";
-
-
     } else {
-
-         /*
-            DARK MODE
-            DARK MODE
-            DARK MODE
-        */
-
         let children = container.children;
         for (let i = 0; i < children.length; i++) {
             children[i].style.color = "#f0f0f0";
@@ -58,11 +38,6 @@ themeToggle.addEventListener('click', function() {
         themeToggle.innerHTML = `<svg fill="#747474" height="24" width="24" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 472.618 472.618" xml:space="preserve" style="--darkreader-inline-fill: #000000;" data-darkreader-inline-fill=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M380.525,337.291c-135.427,0-245.302-109.773-245.302-245.302c0-32.502,6.338-63.575,17.991-91.988 C63.372,36.286,0,124.39,0,227.315c0,135.427,109.875,245.302,245.302,245.302c102.923,0,191.029-63.472,227.316-153.315 C444.201,330.954,413.129,337.291,380.525,337.291z"></path> </g> </g> </g></svg>`;
         isLightMode = false;
         container.style.backgroundColor = "#4b4b4b";
-        document.getElementById('github').innerHTML = `<svg width="48px" height="48px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#f4f4f4" style="--darkreader-inline-fill: #f4f4f4;" data-darkreader-inline-fill=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>github [#142]</title><desc>Created with Sketch.</desc><defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="f4f4f4" fill-rule="evenodd" style="--darkreader-inline-stroke: none;" data-darkreader-inline-stroke=""> <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)" fill="#747474" style="--darkreader-inline-fill: #000000;" data-darkreader-inline-fill=""> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399" id="github-[#142]"> </path> </g> </g> </g> </g></svg>    `;
-        rmbReset.style.backgroundColor = "#b0b0b0";
-        lmbReset.style.backgroundColor = "#b0b0b0";
-        rmbReset.style.color = "#141414";
-        lmbReset.style.color = "#141414";
 
     }
 });
