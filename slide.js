eae6fc7a1aec92d451f7c7a409847e1a62785079
zyqunix <fabiovanek2008@gmail.com>
2024-11-01 15:57:25 +0100

    important

--- slide.js
+++ slide.js
@@ -1,8 +1,8 @@
 let slideIndex = 0;
 let autoScrollTimer;
 const media = [
-    { type: "image", src: "https://cdn.discordapp.com/attachments/1262379758478426122/1300822644509114368/image.png?ex=67223cfe&is=6720eb7e&hm=5c81ba48649c2bc0a4d249ad488d0abaa738d72949d4deb714fda4929d832aa3" },
-    { type: "image", src: "https://cdn.discordapp.com/attachments/1281034862391988304/1300921012539490426/image.png?ex=6722989b&is=6721471b&hm=6fc40dd7272e62c4f2cc4cf73433d4c012d9fa939da32a72a5223f8138d4153e&&" },
+    { type: "image", src: "https://files.catbox.moe/2hhmit.png" },
+    { type: "image", src: "https://files.catbox.moe/6706jd.png" },
     { type: "video", src: "https://files.catbox.moe/qyatl9.mp4" }
 ];
 
