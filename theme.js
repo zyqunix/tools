464282848abe602485f9e8d355d9bfc104e0eee0
zyqunix <fabiovanek2008@gmail.com>
2024-11-19 22:14:11 +0100

    removed sum linez

--- theme.js
+++ theme.js
@@ -24,8 +24,6 @@ themeToggle.addEventListener('click', function () {
             toolContainers.style.borderColor = "#aeaeae";    
         });
 
-
-
         const welcomeChildren = welcome.querySelectorAll('*');
         welcomeChildren.forEach(child => {
             child.style.color = "#1E1E1E";
