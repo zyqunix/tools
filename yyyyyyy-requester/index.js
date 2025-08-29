79d5d90117ccf8a0824e8e0a4c4f775c27bd60e8
zyqunix <e@fbi.one>
2025-08-30 00:25:00 +0200

    yyyyyyy.info requester

---
+++ yyyyyyy-requester/index.js
@@ -0,0 +1,56 @@
+const options = document.getElementById("options");
+const amount = document.getElementById("amount");
+const fetchBtn = document.getElementById("fetch");
+const output = document.getElementById("output")
+
+fetchBtn.addEventListener("click", async function() {
+    await fetchData(options.value, amount.value);
+});
+
+async function fetchData(type, amount) {
+    let body;
+    if (type === 'layer2') {
+        body = JSON.stringify({
+            query: `{ layer(amount: ${amount}, minDim: 0, maxDim: 200) { texts } }`
+        });
+    } else {
+        body = JSON.stringify({ query: `{${type}(amount: ${amount})}` });
+    }
+    const res = await fetch("https://api.yyyyyyy.info/", {
+        method: "POST",
+        headers: {
+            "Authorization": "Bearer 8c33411c58a2d7d0d598e2de2542838c",
+            "Content-Type": "application/json",
+            "accept": "*/*",
+            "authtoken": "8c33411c58a2d7d0d598e2de2542838c"
+        },
+        body
+    });
+    const data = await res.json();
+
+    output.innerHTML = "";
+
+    if (type === "layer2") {
+        console.log(data);
+        const texts = data.data.layer.texts;
+        texts.forEach(text => {
+            const p = document.createElement("p");
+            p.innerHTML = text;
+            output.appendChild(p);
+        });
+    } else {
+        const list = data.data[type];
+        list.forEach(el => {
+            let element;
+            if (type === "background") {
+                element = document.createElement("img");
+                element.src = `https://files.yyyyyyy.info/${el}`;
+            } else {
+                element = document.createElement("video");
+                element.src = `https://files.yyyyyyy.info/${el}`;
+                element.controls = true;
+            }
+            output.appendChild(element);
+        });
+    }
+}
